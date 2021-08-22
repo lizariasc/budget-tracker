12239d6872847a08c02d3a475287cc86a3b3293a
Liz A <lizethariasc14@gmail.com>
2021-08-22 10:39:55 -0700

    intercept fetch requests

--- service-worker.js
+++ service-worker.js
@@ -20,7 +20,7 @@ const FILES_TO_CACHE = [
   ];
 
 
-
+// install 
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
@@ -29,3 +29,44 @@ self.addEventListener('install', function (e) {
         })
       )
     })
+
+// activate
+self.addEventListener('activate', function (e) {
+        e.waitUntil(
+          caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+              return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+            // returns a promise that resolves once all old versions of the cache have been deleted 
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                console.log('deleting cache : ' + keyList[i] );
+                return caches.delete(keyList[i]);
+                }
+            })
+        );
+    })
+    )
+  });
+
+  // retrieving information from the cache 
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+        //   console.log(e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+        //   console.log(e.request.url)
+          return fetch(e.request)
+        }
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+  });
