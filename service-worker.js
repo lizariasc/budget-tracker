b1ff2aaecd71bf350b4288df30d4ea7545b84817
Liz A <lizethariasc14@gmail.com>
2021-08-22 10:32:53 -0700

    install service worker

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,31 @@
-self.addEventListener('install', function (e) {
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
 
-})
+const FILES_TO_CACHE = [
+    '/',
+  '/public/index.html',
+  '/public/assets/js/index.js',
+  '/public/assets/js/idb.js',
+  '/manifest.json',
+  '/public/assets/css/styles.css',
+  '/public/icons/icon-72x72.png',
+  '/public/icons/icon-96x96.png',
+  '/public/icons/icon-128x128.png',
+  '/public/icons/icon-144x144.png',
+  '/public/icons/icon-152x152.png',
+  '/public/icons/icon-192x192.png',
+  '/public/icons/icon-384x384.png',
+  '/public/icons/icon-512x512.png'  
+  ];
+
+
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+          console.log('installing cache : ' + CACHE_NAME)
+          return cache.addAll(FILES_TO_CACHE)
+        })
+      )
+    })
